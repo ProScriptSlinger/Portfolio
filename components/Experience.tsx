395cd49b7fdf38437e1d448ab635928adce8080f
luckyray0314 <saivietthanh0314@gmail.com>
2024-10-18 03:19:00 -0700

    fix: update of experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,6 +1,32 @@
+import {ExperienceItemType, experiences, skills } from "@/utils/experience";
 import React from "react";
 
 const Experience: React.FC<{}> = () => {
+
+  const Skill = ({key, skill}:{key:string, skill:string}) =>{
+    return(
+      <div key={key} className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
+        {skill}
+      </div>
+    )
+  }
+
+  const ExperienceItem = ({key, companyName, position, period, location, description}:{key:number, companyName:string, position:string, period:string, location:string, description:string}) =>{
+    return(
+      <div key={key}>
+        <div className="flex  flex-row justify-between pt-5">
+          <p className="text-gray-300 ">
+            <span className="font-semibold">{`${companyName} /`}</span> {position}
+          </p>
+          <p className="text-gray-300">{`${period}, ${location}`}</p>
+        </div>
+        <p className="text-gray-300 pt-5">
+          {description}
+        </p>
+      </div>
+    )
+  }
+
   return (
     <section id="experience">
       <h1 className="text-white font-semibold text-center text-6xl   pt-[35px]">
@@ -10,118 +36,13 @@ const Experience: React.FC<{}> = () => {
         EXPLORE NOW
       </p>
       <div className=" container mx-auto 2xl ">
-        <div className="flex  flex-row justify-between pt-5">
-          <p className="text-gray-300 ">
-            <span className="font-semibold">Duke Energy /</span> SENIOR
-            FULL-STACK DEVELOPER
-          </p>
-          <p className="text-gray-300">09/2019 - 01/2023, New York, NY</p>
-        </div>
-        <p className="text-gray-300 pt-5">
-          Accomplished Full-Stack Developer with expertise in developing web
-          applications and RESTful APIs using Angular, Nest.js, ASP.NET,
-          SOAP/REST Services, and leveraging various front-end and back-end
-          technologies, design patterns, and DevOps practices to build easily
-          maintainable, scalable, and user-centric applications. Demonstrated
-          proficiency in data management, including designing Snowflake data
-          models, implementing data pipelines and ETL workflows using Apache
-          Airflow, developing optimized SQL queries and data models, and
-          utilizing D3.js for data visualization, while also managing
-          application deployment on cloud platforms like Amazon AWS and Google
-          AppEngine.
-        </p>
-        <div className="flex  flex-row justify-between pt-10">
-          <p className="text-gray-300 ">
-            <span className="font-semibold">Born Group /</span> FULL-STACK
-            DEVELOPER
-          </p>
-          <p className="text-gray-300">12/2017 - 11/2019, Charlotte, NC</p>
-        </div>
-        <p className="text-gray-300 pt-5">
-          Engaged with client stakeholders to gather requirements, estimate user
-          stories, and translate application storyboards and use cases into
-          functional specifications, while developing sub-applications using
-          Nest.js for the backend and Angular for the frontend, incorporating
-          various technologies and adhering to coding best practices.
-          Demonstrated proficiency in integrating SOAP/REST Services, managing
-          build and deployment processes through Jenkins and AWS, conducting
-          thorough testing, providing support to the Operations team, developing
-          Angular JavaScript frameworks, creating and maintaining cloud
-          applications, and collaborating with end-users to redesign legacy
-          applications using the latest technology stack including Nest.js,
-          Angular, and related technologies.
-        </p>
-        <div className="flex  flex-row justify-between pt-10">
-          <p className="text-gray-300 ">
-            <span className="font-semibold">Born Group /</span> BACK-END
-            DEVELOPER
-          </p>
-          <p className="text-gray-300">01/2017 - 11/2017, Chicago, IL</p>
-        </div>
-        <p className="text-gray-300 pt-5">
-          Worked closely with the team to understand requirements, contribute to
-          design and architecture, and write efficient, reusable code using .NET
-          programming languages such as C# and VB.NET, while also assisting in
-          testing, deploying, and improving existing software applications,
-          creating and maintaining documentation, and providing technical
-          support for web, desktop, or mobile applications developed using .NET.
-          Demonstrated versatility in various aspects of the software
-          development life cycle, including requirements gathering, design,
-          development, testing, deployment, and maintenance, showcasing strong
-          technical skills and a collaborative approach.
-        </p>
+        {experiences.map((experience : ExperienceItemType, index ) => (
+          <ExperienceItem key={index} {...experience} />
+        ))}        
         <div className="flex-wrap flex sm:flex-row gap-1">
-          <div className="bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            React
-          </div>
-          <div className="bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            NextJS
-          </div>
-          <div className="bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            JavaScript
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Typescript
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Vue.js
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Responsive Design
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Communication&Teamwork
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Continuous Learning
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Web Performance Optimization
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            AWS
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            web3.js
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Nodejs Framework
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            CI/CD
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            C#
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            .Net
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Django
-          </div>
-          <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Fast Api
-          </div>
+          {skills.map((skill) => (
+            <Skill key={skill} skill={skill} />
+          ))}
         </div>
       </div>
     </section>
---
+++ utils/experience.ts
@@ -0,0 +1,58 @@
+
+export interface ExperienceItemType {
+    companyName: string,
+    position: string,
+    period: string,
+    location: string,
+    description: string
+}
+
+export const experiences: ExperienceItemType[] = [
+    {
+        companyName: "Ahau-x",
+        position: "Leader & Senior Full Stack Developer",
+        period: "10/2023 – Current",
+        location: 'Guatemala ,Guatemala',
+        description: "I developed a CRM site using React and Express.js, focusing on user interface design with HTML5, Tailwind CSS, and Bootstrap. My experience includes implementing advanced object detection techniques for agricultural applications and ensuring code quality through Test Driven Development (TDD) with the NUnit Framework. I utilized Docker and Kubernetes for scalable cloud deployments and integrated SAML and JWT for secure user authentication. Additionally, I embraced DevOps practices with CI/CD pipelines through Jenkins and monitored application performance using Datadog and Splunk. My work also involved collaborating on agile projects with Jira and designing data models in Snowflake while implementing data pipelines and ETL workflows using Apache Airflow."
+    },
+    {
+        companyName: "The house of Yaya",
+        position: "Senior Full Stack Developer",
+        period: "9/2019 – 10/2023",
+        location: 'London, UK',
+        description: "Accomplished Full-Stack Developer with expertise in developing web applications and RESTful APIs using Angular, Nest.js, ASP.NET, SOAP/REST Services, and leveraging various front-end and back-end technologies, design patterns, and DevOps practices to build easily maintainable, scalable, and user-centric applications. Demonstrated proficiency in data management, including designing Snowflake data models, implementing data pipelines and ETL workflows using Apache Airflow, developing optimized SQL queries and data models, and utilizing D3.js for data visualization, while also managing application deployment on cloud platforms like Amazon AWS and Google AppEngine."
+    },
+    {
+        companyName: "Duke Energy",
+        position: "Full Stack Developer",
+        period: "12/2017 – 8/2019",
+        location: 'Charlotte, NC',
+        description: "Engaged with client stakeholders to gather requirements, estimate user stories, and translate application storyboards and use cases into functional specifications, while developing sub-applications using Nest.js for the backend and Angular for the frontend, incorporating various technologies and adhering to coding best practices. Demonstrated proficiency in integrating SOAP/REST Services, managing build and deployment processes through Jenkins and AWS, conducting thorough testing, providing support to the Operations team, developing Angular JavaScript frameworks, creating and maintaining cloud applications, and collaborating with end-users to redesign legacy applications using the latest technology stack including Nest.js, Angular, and related technologies."
+    },
+    {
+        companyName: "Civis Analytics",
+        position: "Backend Developer ",
+        period: "1/2017 – 11/2017",
+        location: 'Chicago, IL',
+        description: "Worked closely with the team to understand requirements, contribute to design and architecture, and write efficient, reusable code using .NET programming languages such as C# and VB.NET, while also assisting in testing, deploying, and improving existing software applications, creating and maintaining documentation, and providing technical support for web, desktop, or mobile applications developed using .NET. Demonstrated versatility in various aspects of the software development life cycle, including requirements gathering, design, development, testing, deployment, and maintenance, showcasing strong technical skills and a collaborative approach."
+    },
+];
+
+export const skills: string[] = [
+    "React",
+    "Next.js",
+    "JavaScript",
+    "Typescript",
+    "Vue.js",
+    "Nodejs Framework",
+    "Communication&Teamwork",
+    "Continuous Learning",
+    "Web Performance Optimization",
+    "AWS",
+    "web3.js",
+    "CI/CD",
+    "C#",
+    ".Net",
+    "Django",
+    "Fast Api"
+]
